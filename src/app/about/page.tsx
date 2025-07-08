369003d6612dc8e9d1f5099b167b68d481ffe0a2
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-07-08 10:38:45 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import About from "../_components/About/About";
 
-
+export const dynamic = 'force-dynamic';
 export default function AboutPage() {
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center">
